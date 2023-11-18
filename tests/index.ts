45d108ca40af4e7939a9822d9f93ae6b996bda7c
Lionello Lunesu <lio+git@lunesu.com>
2023-11-18 14:08:26 +0800

    add integration test

---
+++ tests/index.ts
@@ -0,0 +1,20 @@
+import { DefangService } from "@defang-io/pulumi-defang/lib";
+
+const fromDir = new DefangService("test1", {
+  build: {
+    context: "./test1",
+    dockerfile: "Dockerfile.test",
+    args: {
+      DNS: `defang-test1.prod1.defang.dev`,
+    },
+  },
+});
+
+export const test1etag = fromDir.etag;
+
+// const fromImage = new DefangService("test2", {
+//   image: "nginx:latest",
+//   ports: [{ target: 80, mode: "ingress" }],
+// });
+
+// export const test2url = fromImage.endpoints[0];
