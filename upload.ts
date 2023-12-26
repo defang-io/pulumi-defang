45fe4d477f32122d0e2d07ce912e6f08fa720a40
Lionello Lunesu <lio+git@lunesu.com>
2023-12-26 15:53:55 -0800

    use .dockerignore file

--- upload.ts
+++ upload.ts
@@ -1,48 +1,66 @@
 import { createReadStream, createWriteStream, promises, Stats } from "fs";
 import { tmpdir } from "os";
-import { basename, join, normalize } from "path";
+import { join, normalize } from "path";
 import { promises as stream } from "stream";
 import * as tar from "tar";
+import ignore from "@balena/dockerignore";
+
+const SOURCE_DATE_EPOCH = 315532800; // 1980-01-01, same as nix-shell
+const defaultDockerIgnore = `# Default .dockerignore file for Defang
+**/.DS_Store
+**/.direnv
+**/.envrc
+**/.git
+**/.github
+**/.idea
+**/.vscode
+**/__pycache__
+**/compose.yaml
+**/compose.yml
+**/defang.exe
+**/docker-compose.yaml
+**/docker-compose.yml
+**/node_modules
+**/Thumbs.db
+# Ignore our own binary, but only in the root to avoid ignoring subfolders
+defang`;
 
 const extractMessageRegex = /<Message>(.*?)<\/Message>/;
-
-function filter(path: string, stat: Stats): boolean {
-  // TODO: use .dockerignore
-  switch (basename(path)) {
-    case ".DS_Store":
-    case ".direnv":
-    case ".envrc":
-    case ".git":
-    case ".github":
-    case ".idea":
-    case ".vscode":
-    case "__pycache__":
-    case "defang.exe": // our binary
-    case "docker-compose.yml":
-    case "docker-compose.yaml":
-    case "node_modules":
-    case "Thumbs.db":
-      return false; // omit
-    case "defang": // our binary
-      return stat.isDirectory(); // omit only if it's a file
-    }
-  return true; // keep
-}
 
 export async function createTarball(
   cwd: string,
   dockerfile?: string
 ): Promise<string> {
+  if (!dockerfile) {
+    dockerfile = "Dockerfile";
+  } else {
+    dockerfile = normalize(dockerfile);
+  }
+
+  // A Dockerfile-specific ignore-file takes precedence over the .dockerignore file at the root of the build context if both exist.
+  let patterns: string | undefined;
+  try {
+    patterns = await promises.readFile(
+      join(cwd, dockerfile + ".dockerignore"),
+      "utf8"
+    );
+    console.debug("Using", dockerfile + ".dockerignore");
+  } catch {
+    try {
+      patterns = await promises.readFile(join(cwd, ".dockerignore"), "utf8");
+      console.debug("Using .dockerignore");
+    } catch {
+      console.debug("No .dockerignore file found; using defaults");
+      patterns = defaultDockerIgnore;
+    }
+  }
+  const filter = ignore({ ignorecase: false }).add(patterns).createFilter();
+
   const tempdir = await promises.mkdtemp(join(tmpdir(), "defang-build-"));
   console.debug(`Using temporary folder ${tempdir}`);
   const temppath = join(tempdir, "context.tar.gz");
   try {
     let foundDockerfile = false;
-    if (!dockerfile) {
-      dockerfile = "Dockerfile";
-    } else {
-      dockerfile = normalize(dockerfile);
-    }
 
     // Using stream.pipeline() instead of .pipe() to correctly handle errors
     await stream.pipeline(
@@ -50,10 +68,10 @@ export async function createTarball(
         {
           cwd,
           filter: (p: string, stat: Stats) => (
-            (foundDockerfile ||= normalize(p) === dockerfile), filter(p, stat)
+            (foundDockerfile ||= normalize(p) === dockerfile), filter(p)
           ),
           gzip: true,
-          mtime: new Date(315532800 * 1000), // 1980-01-01 00:00:00 GMT same as Nix
+          mtime: new Date(SOURCE_DATE_EPOCH * 1000), // seconds -> milliseconds
           portable: true,
           strict: true,
         } as any, // cast needed for mtime (bug in tar type definitions)
