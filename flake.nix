d623f5a8cd9348235bf2f0e7305102bfab0a7cc3
Lionello Lunesu <lio+git@lunesu.com>
2024-04-22 11:17:58 -0700

    update dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/41680f83ea143f3ce4bd774ff53cca91f1dac826";#8ba91230a0da929172cf003e7b04efd6dd61b314";
+    nixpkgs.url = "github:NixOS/nixpkgs/eabe8d3eface69f5bb16c18f8662a702f50c20d5";#41680f83ea143f3ce4bd774ff53cca91f1dac826";#8ba91230a0da929172cf003e7b04efd6dd61b314";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
